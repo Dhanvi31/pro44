35920c140fb3e67be5713a5a3c4c07886a1de1ae
Dhanvi31 <79754778+Dhanvi31@users.noreply.github.com>
2021-06-26 15:10:27 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PETER";
+const accessCode2 = "STANLY";
+const accessCode3 = "BATMAN";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T P E R E ", 100,50)
+    fill("lightblue")
+    text("Hint: SpiderMan Real Name !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("N A T Y S L",704,137);
+    fill("lightblue");
+    text("Hint: Who Wrote The Marvel Comics ",702,168);
+    
+    fill("white")
+    textSize(15)
+    text("M A A B T N",100,300);
+    fill("lightblue");
+    text("Hint: The Bat Based Hero ",100,333);
+
+}
